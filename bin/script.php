3335b3042d32de88509eded555a7cc44924dd16b
MrAnyx <1ptitigre@gmail.com>
2019-07-26 22:15:30 +0200

    [feat] model creation

--- bin/script.php
+++ bin/script.php
@@ -10,16 +10,99 @@ class Script extends CLI
     // register options and arguments
     protected function setup(Options $options)
     {
-        $options->setHelp('A very minimal example that does nothing but print a version');
-        $options->registerOption('version', 'print version', 'v');
+        $options->setHelp('php bin/script.php [option] [argument]');
+
+
+        // options
+        $options->registerOption('controller', 'Create a controller followed by <name-controller>', 'c');
+        $options->registerOption('model', 'Create a model followed by <name-model>', 'm');
+        $options->registerOption('view', 'Create a view followed by <name-view>', 'v');
+
+
+        // arguments
+        $options->registerArgument('argument', 'Name of the Model / Controller / View');
+
+
     }
 
     // implement your code
     protected function main(Options $options)
     {
-        if ($options->getOpt('version')) {
-            $this->info('1.1');
-        } else {
+        if ($options->getOpt('model')) {
+
+            $model = ucfirst(strtolower($options->getArgs()[0]));
+
+            if(!is_dir('App/models/'.$model)){
+                mkdir('App/models/'.$model, 0755, true);
+
+                //Model
+                $fp = fopen('App/models/'.$model.'/'.$model.'.php', 'w');
+                $modelContent = implode(PHP_EOL, [
+                    '<?php',
+                    'namespace App\\'.$model.';',
+                    '',
+                    'class '.$model.' {',
+                    '',
+                    '   //Put the different attributes here as private attributes',
+                    '',
+                    '   public function __construct(array $arrayOfValues = null){',
+                    '       if($arrayOfValues != null){',
+                    '           $this->hydrate($arrayOfValues);',
+                    '       }',
+                    '   }',
+                    '',
+                    '   public function hydrate(array $donnees){',
+                    '       foreach($donnees as $key => $value){,',
+                    '           $method = "set".ucfirst($key);',
+                    '           if(method_exists($this,$method)){',
+                    '               $this->$method($value);',
+                    '           }',
+                    '       }',
+                    '   }',
+                    '',
+                    '   //Getters and Setters',
+                    '}',
+                ]);
+                fwrite($fp, $modelContent);
+                fclose($fp);
+
+                //ModelRepository
+                $fp = fopen('App/models/'.$model.'/'.$model.'Repository.php', 'w');
+                $modelRepoContent = implode(PHP_EOL, [
+                    '<?php',
+                    'namespace App\\'.$model.';',
+                    '',
+                    'use PDO;',
+                    'use App\\'.$model.'\\'.$model.';',
+                    'use App\\Repository\\Repository;',
+                    '',
+                    'class '.$model.'Repository {',
+                    '',
+                    '   private $db;',
+                    '',
+                    '   public function __construct(Repository $db){',
+                    '       $this->db = $db;',
+                    '   }',
+                    '',
+                    '   //Put the different functions here as public functions',
+                    '',
+                    '}',
+                ]);
+                fwrite($fp, $modelRepoContent);
+                fclose($fp);
+
+
+                $this->info('Model successfully created'.PHP_EOL.PHP_EOL.$model.'.php file created here : App/models/'.$model.'/'.$model.'.php'.PHP_EOL.$model.'Repository.php file created here : App/models/'.$model.'/'.$model.'Repository.php');
+
+            }else{
+                $this->info('This model already exists');
+            }
+
+
+
+            //utiliser json_decode pour modifier le fichier compser.json
+            //$this->info(var_dump($options->getArgs()));
+        }else {
             echo $options->help();
         }
     }
