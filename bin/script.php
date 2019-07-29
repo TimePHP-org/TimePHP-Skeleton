03e356a0bc38a64eb320e0b51f6639e30c935f98
MrAnyx <1ptitigre@gmail.com>
2019-07-29 12:49:05 +0200

    [update] adding script controller

--- bin/script.php
+++ bin/script.php
@@ -16,7 +16,7 @@ class Script extends CLI
         $options->registerOption('model', 'Create a model followed by <name-model>', 'm');
         $options->registerOption('view', 'Create a view followed by <name-view>', 'v');
 
-        
+
         // arguments
         $options->registerArgument('argument', 'Name of the Model / Controller / View');
 
@@ -62,6 +62,36 @@ class Script extends CLI
 
             }else{
                 $this->info('This model already exists');
+            }
+
+        }else if ($options->getOpt('controller')) {
+            $controller = ucfirst(strtolower($options->getArgs()[0]));
+
+            $pathTemplate = dirname(__DIR__).DIRECTORY_SEPARATOR.'bin'.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'controllers'.DIRECTORY_SEPARATOR;
+            $path = dirname(__DIR__).DIRECTORY_SEPARATOR.'App'.DIRECTORY_SEPARATOR.'controllers'.DIRECTORY_SEPARATOR;
+            $pathComposer = dirname(__DIR__).DIRECTORY_SEPARATOR;
+
+            if(!is_dir($path.$controller)){
+                mkdir($path.$controller, 0755, true);
+
+                //Controller
+                $fichier = fopen($path.$controller.DIRECTORY_SEPARATOR.$controller.'.php', 'w');
+                $controllerContent = file_get_contents($pathTemplate.'templateController.php');
+                $controllerContent = str_replace("TemplateController" ,$controller, $controllerContent);
+                fwrite($fichier, $controllerContent);
+                fclose($fichier);
+
+                $fileJson = file_get_contents($pathComposer.'composer.json');
+                $composerJson = json_decode($fileJson, true);
+                $composerJson["autoload"]["psr-4"]["App\\".$controller.'\\'] = 'App/controllers/'.$controller.'/';
+                $fichier = fopen($pathComposer.'composer.json', 'w');
+                fwrite($fichier, str_replace('\/', '/', json_encode($composerJson, JSON_PRETTY_PRINT)));
+                fclose($fichier);
+
+                $this->info('Controller successfully created'.PHP_EOL.PHP_EOL.$controller.'.php file created here : App/controllers/'.$controller.'/'.$controller.'.php');
+
+            }else{
+                $this->info('This controller already exists');
             }
 
         }else {
---
+++ bin/templates/views/templateView.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . '/commons/header.php';
+require_once __DIR__ . '/commons/navbar.php';
+
+//uses
+
+// variables
+?>
+
+
+
+
+<?php
+require_once __DIR__ . '/commons/footer.php';
