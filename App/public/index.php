2c0b22d75cb0a9f2ceff298daeb45980eb769c1c
MrAnyx <1ptitigre@gmail.com>
2020-01-13 10:45:37 +0100

    [doc] index.php

--- App/public/index.php
+++ App/public/index.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Entry point of the app
+ * 
+ * PHP version 7.4.1
+ * 
+ * @category Router
+ * @author Robin Bidanchon <robin.bidanchon@gmail.com>
+ * @license MPL-2.0
+ * @link http://domaine.com
+ */
+
 // it loads my classes and the file functions.php thanks to the composer.json file
 require __DIR__ . "/../../vendor/autoload.php";
 
@@ -9,8 +20,8 @@ $router = new Router();
 
 // Routes disponibles
 $router
-    ->get("/", "HomeController#getUsers", "home")
-    ->get("/user/[i:idUser]", "HomeController#getArticleByUser", "articleByUser")
-    ->get("/article/[i:idArticle]/[*:slug]", "HomeController#getFullArticle", "fullArticle")
-    ->run();
+->get("/", "HomeController#getUsers", "home")
+->get("/user/[i:idUser]", "HomeController#getArticleByUser", "articleByUser")
+->get("/article/[i:idArticle]/[*:slug]", "HomeController#getFullArticle", "article")
+->run();
 
