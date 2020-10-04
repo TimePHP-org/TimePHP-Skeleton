585612b7921347725cf59f9c280f220958934c31
MrAnyx <1ptitigre@gmail.com>
2020-10-04 22:51:53 +0200

    [fix] Fixed index.php (entry point)

--- App/public/index.php
+++ App/public/index.php
@@ -10,9 +10,6 @@ require __DIR__ . "/../../config/bootstrap.php";
 use TimePHP\Foundation\Router;
 use App\Bundle\Services\TestService;
 
-
 $router = new Router($options, $twig->getRenderer(), $container);
-
-
 
 $router->initialize($routes)->run();
